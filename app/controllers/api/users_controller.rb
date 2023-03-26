b513780b474064336a73ca98a4b0755911a9d394
Seif <seifMostafaa@bitbucket.com>
2023-03-26 23:14:31 +0200

    users controller addons

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::UsersController < ApplicationController
   before_action :authenticate_and_set_user
   before_action :check_user_role
+  before_action :find_user
 
   # GET /users
   def index
@@ -13,28 +14,21 @@ class Api::UsersController < ApplicationController
     render json: @user, status: :ok
   end
 
-  # POST /users
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user, status: :created
-    else
-      render json: { errors: @user.errors.full_messages },
-             status: :unprocessable_entity
-    end
-  end
-
   # PUT /users/{username}
   def update
     unless @user.update(user_params)
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
+    
+    else
+      render json: @user, status: :ok
     end
   end
 
   # DELETE /users/{username}
   def destroy
     @user.destroy
+    render json: @user, status: :ok
   end
 
   protected
@@ -48,12 +42,12 @@ class Api::UsersController < ApplicationController
   private
 
   def find_user
-    @user = User.find_by_username!(params[:_username])
+    @user = User.where(id: params[:id]).first
   rescue ActiveRecord::RecordNotFound
     render json: { errors: 'User not found' }, status: :not_found
   end
 
   def user_params
-    params.permit(:name, :username, :email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
